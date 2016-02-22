c99a66ac5cc1e58179191dd31a1626a06902b62c
davidmcgirt <davidmcgirt@gmail.com>
2016-02-22 23:07:40 +0000

    SVG animation refinements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,24 +18,61 @@
 //= require_tree .
 
 
+/*  A page can't be manipulated safely until the document is "ready." -> $(document).ready()
+    jQuery detects this state of readiness for you. 
+    This top level funct only runs once pg Document Object Model (DOM) is ready for JS to execute. */ 
 $(function() {
   
-  $(".svg").click(function() {
+  blink(); //Called by the page DOM 
+
+  /* function which animates the blinking effect of the image */
+  function blink() {
+    $(".eyes").velocity({ ry: 0 })
+              .velocity("reverse", { delay: 50 })
+              .delay(3000)
+              .animate({ width:""}, 0, 0, blink); //blink param calls the func when animation completes
+  }
+  
+  /* button click func that animates the svg image  */
+  $(".animate").click(function() {
+   
+   /* animate the chart tilt 5 times */ 
+    var i;
     
-    $("#bar1").velocity({ y: "42.2", height: 7}, [1, 1], 4000 );
-              
-    $("#bar2").velocity({ y: "36.4", height: 17}, [1, 1], 4000 );
-            
-    $("#bar3").velocity({ y: "27.4", height: 32}, [1, 1], 4000 );
-              
-    $("#bar4").velocity({ y: "53.1", height: 6}, [1, 1], 4000 );
+    for (i = 0; i <= 4; i++) {
+      $("#graph").velocity({ rotateZ: "4deg" }).velocity("reverse", { delay: 150 });
+    }
     
-    $("#graph").delay(2000).velocity({ rotateZ: "4deg" }).velocity("reverse", {delay: 150})
-               .velocity({ rotateZ: "4deg" }).velocity("reverse", { delay: 150 });
+    /* animate the side shift 3 times */
+    var k;
+  
+    for (k = 0; k <= 2; k++) {
+      $("#Layer_1").velocity({ translateX: "50px" }, 500);
+      $("#Layer_1").velocity({ translateX: "-50px" }, 500);
+    }
                
-    $("#Layer_1").velocity({ rotateZ: "360deg" }, 1500);
-               
+    //$("#Layer_1").velocity({ rotateZ: "360deg" }, 1500);
+    $("#Layer_1").velocity({ translateX: "0px" }, 1000); //reset the background shift to initial
+    
+  });
+  
+  /* mouse hover func which animates the svg image when hovered, reverses animation when not */
+  $(".svg").hover(function() {
+    
+    /* animate the bar graphs */
+    $("#bar1").velocity({ y: "42.2", height: 7}, [1, 1], 3000 );
+    $("#bar2").velocity({ y: "36.4", height: 17}, [1, 1], 3000 );
+    $("#bar3").velocity({ y: "27.4", height: 32}, [1, 1], 3000 );
+    $("#bar4").velocity({ y: "53.1", height: 6}, [1, 1], 3000 );
+    
+  }, function() {
+    
+    /* when svg not hovered, remove the bar animation effects */
+    $("#bar1").velocity("reverse", { delay: 150 });
+    $("#bar2").velocity("reverse", { delay: 150 });
+    $("#bar3").velocity("reverse", { delay: 150 });
+    $("#bar4").velocity("reverse", { delay: 150 });
+    
   });
   
 });
-
