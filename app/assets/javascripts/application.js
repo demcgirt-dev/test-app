44b2c65b9ccb6091a687a5aee4f0eb1c4ea9fc40
davidmcgirt <davidmcgirt@gmail.com>
2016-05-02 20:04:42 +0000

    Melinda Walsh icon animation dev

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,30 +20,43 @@
 
 /*  A page can't be manipulated safely until the document is "ready." -> $(document).ready()
     jQuery detects this state of readiness for you. 
-    This top level funct only runs once pg Document Object Model (DOM) is ready for JS to execute. */ 
+    This top level funct only runs once pg Document Object Model (DOM) is ready for JS to execute. */
+    
 $(function() {
-  
-  blink(); //Called by the page DOM 
 
-  /* function which animates the blinking effect of the image */
+  blink(); //Called by the page DOM
+
+  // function which animates the blinking effect of the image 
   function blink() {
     $(".eyes").velocity({ ry: 0 })
               .velocity("reverse", { delay: 50 })
-              .delay(3000)
+              .delay(1000)
               .animate({ width:""}, 0, 0, blink); //blink param calls the func when animation completes
   }
   
-  /* button click func that animates the svg image  */
+  
+  /* --------------------------------------------------    
+     MELINDA'S ICON FUNCTION (reference MW dev site) 
+     -------------------------------------------------- */
+     
+     
+  // function which creates the pulsing effect on the icon 
+  function pulse() {
+    	$(".mw_svg").velocity({ scale: 1.1 }, { duration: 1000, loop: 2 })
+    	            .animate({ }, 0, 0, pulse);
+  }
+  
+  // button click func that animates the svg image  
   $(".animate").click(function() {
-   
-   /* animate the chart tilt 5 times */ 
+    
+    // animate the chart tilt 5 times 
     var i;
     
     for (i = 0; i <= 4; i++) {
       $("#graph").velocity({ rotateZ: "4deg" }).velocity("reverse", { delay: 150 });
     }
     
-    /* animate the side shift 3 times */
+    // animate the side shift 3 times 
     var k;
   
     for (k = 0; k <= 2; k++) {
@@ -59,7 +72,7 @@ $(function() {
   /* mouse hover func which animates the svg image when hovered, reverses animation when not */
   $(".svg").hover(function() {
     
-    /* animate the bar graphs */
+    // animate the bar graphs 
     $("#bar1").velocity({ y: "42.2", height: 7}, [1, 1], 3000 );
     $("#bar2").velocity({ y: "36.4", height: 17}, [1, 1], 3000 );
     $("#bar3").velocity({ y: "27.4", height: 32}, [1, 1], 3000 );
@@ -67,12 +80,29 @@ $(function() {
     
   }, function() {
     
-    /* when svg not hovered, remove the bar animation effects */
-    $("#bar1").velocity("reverse", { delay: 150 });
-    $("#bar2").velocity("reverse", { delay: 150 });
-    $("#bar3").velocity("reverse", { delay: 150 });
-    $("#bar4").velocity("reverse", { delay: 150 });
+      /* when svg not hovered, remove the bar animation effects */
+      $("#bar1").velocity("reverse", { delay: 150 });
+      $("#bar2").velocity("reverse", { delay: 150 });
+      $("#bar3").velocity("reverse", { delay: 150 });
+      $("#bar4").velocity("reverse", { delay: 150 });
     
-  });
+    });
   
+  
+  /* --------------------------------------------------    
+     MELINDA'S ICON ANIMATION (reference MW dev site) 
+     -------------------------------------------------- */
+  
+  
+	$(".mw_svg").hover(function() {
+  
+  		$(".mw_svg").velocity({ scale: 1.1 }, { duration: 1000, loop: 2 })
+  		            .animate({ }, 0, 0, pulse); //blink param calls the func when animation completes
+  		
+	}, function() {
+      
+      $(".mw_svg").stop(true, true);	  
+
+	});
+	
 });
